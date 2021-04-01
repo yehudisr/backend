6a4367ae2c78d11ca73163b9e881e92a7fdd8221
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-01 13:30:47 -0500

    testing controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  resources :anecdotes
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  # resources :anecdotes
   # resources :family_members
   # resources :users
+   
 
   get '/users', to: 'users#index'
   get 'users/:id', to: 'users#show'
 
   get 'family_members', to:'family_members#index'
+  post 'family_members', to: 'family_members#create'
+  
   get 'family_members/:id', to:'family_members#show'
+  post 'family_members/:id/anecdotes', to: 'anecdotes#create'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
